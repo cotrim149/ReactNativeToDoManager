d40622b27d7682a9d4823b2a60c275415be17d0a
Victor Cotrim <cotrim149@gmail.com>
2018-05-17 11:12:58 -0300

    Change App.js to a new folder and configure index.js of initial component

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
 
+// Aqui se faz o registro do ponto inicial do app
 AppRegistry.registerComponent('ToDoManager', () => App);
